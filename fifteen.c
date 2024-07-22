a7de239065273a819121880511237cdedb45101d
dantekadagi3 <dantekadagi3@gmail.com>
2024-07-22 11:16:26 +0300

    updates

--- fifteen.c
+++ fifteen.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-int main() {
+
+int main(){
     char str[100];
-    printf("Enter a line of text: ");
-    fgets(str, sizeof(str), stdin);
-    printf("You entered: %s", str);
+    printf("Enter a line of text ");
+    fgets(str,sizeof(str),stdin);
+    printf("You entered : %s",str);
     return 0;
 }
